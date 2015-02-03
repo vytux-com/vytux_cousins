9e412b3dc285bcfc8abe5b79ced578f9bf1c38b9
Vytautas Krivickas <vytautas.krivickas@gmail.com>
2015-02-03 19:20:12 +1100

    Fix compatibility with latest class changes

--- module.php
+++ module.php
@@ -28,6 +28,7 @@ namespace Webtrees;
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
+use PDO;
 
 class vytux_cousins_WT_Module extends Module implements ModuleTabInterface {
 /*
@@ -127,21 +128,21 @@ class vytux_cousins_WT_Module extends Module implements ModuleTabInterface {
 		}
 
 		//Lookup father's siblings
-		$rows=Database::prepare("SELECT l_to as xref FROM `##link` WHERE l_file = ".WT_GED_ID." AND l_type LIKE 'CHIL' AND l_from LIKE '".substr($grandparentFamilyHusb, 0, strpos($grandparentFamilyHusb, '@'))."'")->fetchAll(); //PDO::FETCH_ASSOC);
+		$rows=Database::prepare("SELECT l_to as xref FROM `##link` WHERE l_file = ".WT_GED_ID." AND l_type LIKE 'CHIL' AND l_from LIKE '".substr($grandparentFamilyHusb, 0, strpos($grandparentFamilyHusb, '@'))."'")->fetchAll(PDO::FETCH_ASSOC);
 		foreach ($rows as $row) {
 			if ($row['xref'] != substr($parentFamily->getHusband(), 0, strpos($parentFamily->getHusband(), '@')))
 				$list_f[]=$row['xref'];
 		}
 		//Lookup Aunt & Uncle's families (father's family)
 		foreach ($list_f as $ids) {
-			$rows=Database::prepare("SELECT l_from as xref FROM `##link` WHERE l_file = ".WT_GED_ID." AND (l_type LIKE 'HUSB' OR l_type LIKE 'WIFE') AND l_to LIKE '".$ids."'")->fetchAll();//PDO::FETCH_ASSOC);
+			$rows=Database::prepare("SELECT l_from as xref FROM `##link` WHERE l_file = ".WT_GED_ID." AND (l_type LIKE 'HUSB' OR l_type LIKE 'WIFE') AND l_to LIKE '".$ids."'")->fetchAll(PDO::FETCH_ASSOC);
 			foreach ($rows as $row) {
 				$list_f2[]=$row['xref'];
 			}
 		}
 		//Lookup cousins (father's family)
 		foreach ($list_f2 as $id2) {
-			$rows=Database::prepare("SELECT l_to as xref FROM `##link` WHERE l_file = ".WT_GED_ID." AND l_type LIKE 'CHIL' AND l_from LIKE '".$id2."'")->fetchAll();//PDO::FETCH_ASSOC);
+			$rows=Database::prepare("SELECT l_to as xref FROM `##link` WHERE l_file = ".WT_GED_ID." AND l_type LIKE 'CHIL' AND l_from LIKE '".$id2."'")->fetchAll(PDO::FETCH_ASSOC);
 			foreach ($rows as $row) {
 				$list_f3[]=$row['xref'];
 				$count_cousins_f ++;
@@ -149,21 +150,21 @@ class vytux_cousins_WT_Module extends Module implements ModuleTabInterface {
 		}
 
 		//Lookup mother's siblings
-		$rows=Database::prepare("SELECT l_to as xref FROM `##link` WHERE l_file = ".WT_GED_ID." AND l_type LIKE 'CHIL' AND l_from LIKE '".substr($grandparentFamilyWife, 0, strpos($grandparentFamilyWife, '@'))."'")->fetchAll();//PDO::FETCH_ASSOC);
+		$rows=Database::prepare("SELECT l_to as xref FROM `##link` WHERE l_file = ".WT_GED_ID." AND l_type LIKE 'CHIL' AND l_from LIKE '".substr($grandparentFamilyWife, 0, strpos($grandparentFamilyWife, '@'))."'")->fetchAll(PDO::FETCH_ASSOC);
 		foreach ($rows as $row) {
 			if ($row['xref'] != substr($parentFamily->getWife(), 0, strpos($parentFamily->getWife(), '@')))
 				$list_m[]=$row['xref'];
 		}
 		//Lookup Aunt & Uncle's families (mother's family)
 		foreach ($list_m as $ids) {
-			$rows=Database::prepare("SELECT l_from as xref FROM `##link` WHERE l_file = ".WT_GED_ID." AND (l_type LIKE 'HUSB' OR l_type LIKE 'WIFE') AND l_to LIKE '".$ids."'")->fetchAll();//PDO::FETCH_ASSOC);
+			$rows=Database::prepare("SELECT l_from as xref FROM `##link` WHERE l_file = ".WT_GED_ID." AND (l_type LIKE 'HUSB' OR l_type LIKE 'WIFE') AND l_to LIKE '".$ids."'")->fetchAll(PDO::FETCH_ASSOC);
 			foreach ($rows as $row) {
 				$list_m2[]=$row['xref'];
 			}
 		}
 		//Lookup cousins (mother's family)
 		foreach ($list_m2 as $id2) {
-			$rows=Database::prepare("SELECT l_to as xref FROM `##link` WHERE l_file = ".WT_GED_ID." AND l_type LIKE 'CHIL' AND l_from LIKE '".$id2."'")->fetchAll();//PDO::FETCH_ASSOC);
+			$rows=Database::prepare("SELECT l_to as xref FROM `##link` WHERE l_file = ".WT_GED_ID." AND l_type LIKE 'CHIL' AND l_from LIKE '".$id2."'")->fetchAll(PDO::FETCH_ASSOC);
 			foreach ($rows as $row) {
 				$list_m3[]=$row['xref'];
 				$count_cousins_m ++;
@@ -201,7 +202,7 @@ class vytux_cousins_WT_Module extends Module implements ModuleTabInterface {
 					}
 				}
 				if (isset($pedi) && $pedi != 'birth') {
-					$label = '<span class="cousins_pedi">'.Gedcom_Code_Pedi::getValue($pedi, $record).'</span>';
+					$label = '<span class="cousins_pedi">'.WT_Gedcom_Code_Pedi::getValue($pedi, $record).'</span>';
 				}
 				if ($cousinParentFamily != $prev_fam_id) {
 					$prev_fam_id = $cousinParentFamily;
@@ -244,7 +245,7 @@ class vytux_cousins_WT_Module extends Module implements ModuleTabInterface {
 					}
 				}
 				if (isset($pedi) && $pedi != 'birth') {
-					$label = '<span class="cousins_pedi">'.Gedcom_Code_Pedi::getValue($pedi, $record).'</span>';
+					$label = '<span class="cousins_pedi">'.WT_Gedcom_Code_Pedi::getValue($pedi, $record).'</span>';
 				}
 				if ($cousinParentFamily != $prev_fam_id) {
 					$prev_fam_id = $cousinParentFamily;
